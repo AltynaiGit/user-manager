480fb1537c95a3de497ed4ac8042b6e0fb30aafc
AltynaiGit <altynai.777.777.777@gmail.com>
2026-02-16 16:29:30 +0600

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    static void main() {
+    static void main() throws IOException {
 
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Напишите ваше имя: ");
+        String user = scn.nextLine();
 
+        File file = new File("C:\\Users\\Elite X2\\Downloads\\users.txt");
+
+        FileWriter writer = new FileWriter(file, true);
+        writer.write(user + "\n");
+        writer.close();
     }
 }
